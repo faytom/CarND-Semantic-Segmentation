bab3d7be7c6e012403fde9b53fec910397c7d257
Tom Zheng <tom.zheng@maxustech.com>
2018-02-21 07:24:27 +0800

    (*)Uncommented GPU check

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ import project_tests as tests
 assert LooseVersion(tf.__version__) >= LooseVersion('1.0'), 'Please use TensorFlow version 1.0 or newer.  You are using {}'.format(tf.__version__)
 print('TensorFlow Version: {}'.format(tf.__version__))
 
-# Check for a GPU
-# if not tf.test.gpu_device_name():
-#     warnings.warn('No GPU found. Please use a GPU to train your neural network.')
-# else:
-#     print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
+Check for a GPU
+if not tf.test.gpu_device_name():
+    warnings.warn('No GPU found. Please use a GPU to train your neural network.')
+else:
+    print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
 
 
 def load_vgg(sess, vgg_path):
