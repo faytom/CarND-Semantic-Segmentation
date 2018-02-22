092dbbeadc949301c7c844df27e88668479efc48
Tom Zheng <tom.zheng@maxustech.com>
2018-02-23 00:09:30 +0800

    (+)Added label reshape

--- main.py
+++ main.py
@@ -88,6 +88,8 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     """
     # TODO: Implement function
     logits = tf.reshape(nn_last_layer, (-1, num_classes))
+    correct_label = tf.reshape(correct_label, (-1,num_classes))
+    
     cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=correct_label))
     
     optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
